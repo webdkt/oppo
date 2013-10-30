4eaa6daf7513a92bfd8e07f0ce8c3565a3171aa5
webdkt <webdkt@gmail.com>
2013-10-30 19:32:30 +0800

    form test

--- mysite/lawaccount/templatetags/bootstrap.py
+++ mysite/lawaccount/templatetags/bootstrap.py
@@ -1,16 +1,23 @@
 from django.template import Context
 from django.template.loader import get_template
 from django import template
+import sys
 
 register = template.Library()
 
-APP_NAME = 'lawaccount'
+TEMPLATE_PATH = 'lawaccount/'
 
 @register.filter
 def bootstrap(element, template_name='form.html'):
     markup_classes = {'label': '', 'value': '', 'single_value': ''}
+    print >>sys.stderr, 'doing bootstrap:' + template_name
     return render(element, markup_classes, template_name)
 
+
+@register.filter
+def bs3field(element):
+    markup_classes = {'label': 'col-md-3', 'value': 'col-md-9', 'single_value': ''}
+    return render(element, markup_classes, 'form_field.html')
 
 @register.filter
 def bootstrap_inline(element, template_name='form.html'):
@@ -52,8 +59,9 @@ def bootstrap_horizontal(element, label_cols={},template_name='form.html'):
 def add_input_classes(field):
     if not is_checkbox(field) and not is_multiple_checkbox(field) and not is_radio(field):
         field_classes = field.field.widget.attrs.get('class', '')
-        field_classes += ' form-control'
-        field.field.widget.attrs['class'] = field_classes
+        if not ('form-control' in field_classes):
+            field_classes += ' form-control'
+            field.field.widget.attrs['class'] = field_classes
 
 
 def render(element, markup_classes, template_name):
@@ -61,7 +69,7 @@ def render(element, markup_classes, template_name):
 
     if element_type == 'boundfield':
         add_input_classes(element)
-        template = get_template(APP_NAME +"/field.html")
+        template = get_template(TEMPLATE_PATH + template_name)
         context = Context({'field': element, 'classes': markup_classes})
     else:
         has_management = getattr(element, 'management_form', None)
@@ -70,13 +78,13 @@ def render(element, markup_classes, template_name):
                 for field in form.visible_fields():
                     add_input_classes(field)
 
-            template = get_template(APP_NAME + "formset.html")
+            template = get_template(TEMPLATE_PATH + "formset.html")
             context = Context({'formset': element, 'classes': markup_classes})
         else:
             for field in element.visible_fields():
                 add_input_classes(field)
 
-            template = get_template(APP_NAME + "/" + template_name)
+            template = get_template(TEMPLATE_PATH + template_name)
             context = Context({'form': element, 'classes': markup_classes})
 
     return template.render(context)
